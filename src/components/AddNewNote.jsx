277fa34e5da67cb2ed199bc6495d6d3d083aa31e
itsSoheil <soheil.samadi79@gmail.com>
2024-02-06 13:35:16 +0330

    build AddNewNote Component With States

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+
 function AddNewNote() {
-  return <div className="add-new-note"></div>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [note, setNote] = useState([]);
+
+  const handdleSubmit = (event) => {
+    event.preventDefault();
+    console.log("click shode!");
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    console.log(newNote);
+    setTitle("");
+    setDescription("");
+    setNote((prevNote) => [...prevNote, newNote]);
+  };
+
+  return (
+    <div className="add-new-note">
+      <h2>Add New Note</h2>
+      <form className="note-form" onSubmit={handdleSubmit}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="Note Title"
+        />
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="Note Description"
+        />
+        <button type="submit" className="btn btn--primary">
+          Add New Note
+        </button>
+      </form>
+    </div>
+  );
 }
 export default AddNewNote;
