4d839afb858bf63548af0f5f0a0031907fbf0e93
itsSoheil <soheil.samadi79@gmail.com>
2024-02-06 11:35:18 +0330

    Building The General Of The NoteApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import "./App.css";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return (
+    <div className="container">
+      <div className="note-header"></div>
+      <div className="note-app">
+        <div className="add-new-note"></div>
+        <div className="note-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
