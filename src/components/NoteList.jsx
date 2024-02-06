162f5f1b9e979558f6fce103ec8583f30e359398
itsSoheil <soheil.samadi79@gmail.com>
2024-02-06 15:05:37 +0330

    Create NoteItem In NoteList

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,9 +1,37 @@
-function NoteList() {
+function NoteList({ notes }) {
   return (
     <div className="note-container">
-      <h1>Lorem ipsum dolor sit amet.</h1>
+      {notes.map((item) => (
+        <NoteItem key={item.id} note={item} />
+      ))}
     </div>
   );
 }
 
 export default NoteList;
+
+function NoteItem({ note }) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return (
+    <div className="note-item">
+      <div className="note-item__header">
+        <div>
+          <p className="title">{note.title}</p>
+          <p className="desc">{note.description}</p>
+        </div>
+        <div className="actions">
+          <button>X</button>
+          <input type="checkbox" />
+        </div>
+      </div>
+      <div className="note-item__footer">
+        {new Date(note.createdAt).toLocaleDateString("en-US", options)}
+      </div>
+    </div>
+  );
+}
