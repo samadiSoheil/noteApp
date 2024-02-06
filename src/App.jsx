061ccf2e1a420220868cdebf1de4f9c3e075fb66
itsSoheil <soheil.samadi79@gmail.com>
2024-02-06 13:58:02 +0330

    Take The Notes State And Update It

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 function App() {
+  const [notes, setNotes] = useState([]);
+  const handleAddNotes = (newNote) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
   return (
     <div className="container">
       <div className="note-header"></div>
       <div className="note-app">
-        <AddNewNote />
+        <AddNewNote onAddNote={handleAddNotes} />
         <div className="note-container"></div>
       </div>
     </div>
