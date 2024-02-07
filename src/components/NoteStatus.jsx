b518c8b8dca52c705fd2dc864ed3d30d6145e4d6
itsSoheil <soheil.samadi79@gmail.com>
2024-02-07 22:05:38 +0330

    Coplete NoteStatus Component

--- src/components/NoteStatus.jsx
+++ src/components/NoteStatus.jsx
@@ -1,7 +1,21 @@
 function NoteStatus({ notes }) {
+  let allNotes = notes.length;
+  let completedNotes = notes.filter((n) => n.completed).length;
+  let openNotes = allNotes - completedNotes;
+
+  if (!allNotes) return <h2>Please Enter A Note</h2>;
+
   return (
     <ul className="note-status">
-      <h1>Lorem ipsum dolor sit amet.</h1>
+      <li>
+        All <span>{allNotes}</span>
+      </li>
+      <li>
+        Completed <span>{completedNotes}</span>
+      </li>
+      <li>
+        Open <span>{openNotes}</span>
+      </li>
     </ul>
   );
 }
