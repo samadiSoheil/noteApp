b7b5cdc7912108dca61d8e76b143e9a667d4a2a3
itsSoheil <soheil.samadi79@gmail.com>
2024-02-07 22:32:20 +0330

    Create NoteHeader Component With State

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function NoteHeader({ notes }) {
-  return <div className="note-header"></div>;
+  const [sortBy, setSortBy] = useState("latest");
+  console.log(sortBy);
+  return (
+    <div className="note-header">
+      <h1>My Notes {notes.length}</h1>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="latest">Sort based on latest notes</option>
+        <option value="earliest">Sort based on earliest notes</option>
+        <option value="completed">Sort based on completed notes</option>
+      </select>
+    </div>
+  );
 }
 
 export default NoteHeader;
